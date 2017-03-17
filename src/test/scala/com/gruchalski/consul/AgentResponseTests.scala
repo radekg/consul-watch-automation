1826bbb415c539bc4832ccaefe910acef4e8260d
radekg <radek@gruchalski.com>
2017-03-17 23:31:36 +0100

    event watch model

--- src/test/scala/com/gruchalski/consul/AgentResponseTests.scala
+++ src/test/scala/com/gruchalski/consul/AgentResponseTests.scala
@@ -1,12 +1,13 @@
 package com.gruchalski.consul
 
-import com.gruchalski.consul.models.{ServiceWatch, ServiceWatchNode, ServiceWatchParser, ServiceWatchService}
+import com.gruchalski.consul.models._
 import org.scalatest.{Inside, Matchers, WordSpec}
 import play.api.libs.json.Json
 
 class AgentResponseTests extends WordSpec
   with Matchers
   with ServiceWatchParser
+  with EventWatchParser
   with Inside {
 
   "Agent response" must {
@@ -59,6 +60,32 @@ class AgentResponseTests extends WordSpec
             list.head.node shouldBe ServiceWatchNode("foobar", "10.1.10.12")
             list.head.service shouldBe ServiceWatchService("redis", "redis", None, 8000)
             list.head.checks.get.size shouldBe 2
+        }
+
+      }
+
+      "given valid event declaration response" in {
+
+        val jsonData =
+          """
+            |[
+            |  {
+            |    "ID": "f07f3fcc-4b7d-3a7c-6d1e-cf414039fcee",
+            |    "Name": "web-deploy",
+            |    "Payload": "MTYwOTAzMA==",
+            |    "NodeFilter": "",
+            |    "ServiceFilter": "",
+            |    "TagFilter": "",
+            |    "Version": 1,
+            |    "LTime": 18
+            |  }
+            |]
+          """.stripMargin
+
+        inside(Json.parse(jsonData).asOpt[List[EventWatch]]) {
+          case Some(list) =>
+            list.head.id shouldBe "f07f3fcc-4b7d-3a7c-6d1e-cf414039fcee"
+            list.head.ltime shouldBe 18L
         }
 
       }
